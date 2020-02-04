444a3fb8d85b1c9d3c00c9a8696d55ef7a6740ea
soldesk <soldesk@soldesk-PC>
2020-02-04 11:10:10 +0900

    index.do

--- src/main/java/dev/mvc/mealkit/HomeController.java
+++ src/main/java/dev/mvc/mealkit/HomeController.java
@@ -1,39 +1,41 @@
 package dev.mvc.mealkit;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-/**
- * Handles requests for the application home page.
- */
+import org.springframework.web.servlet.ModelAndView;
+ 
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public ModelAndView home1() {
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("redirect:/index.jsp"); // 파일 확장자 포함
+    
+    return mav;
+  }
+  
+  @RequestMapping(value = "/home.do", method = RequestMethod.GET)
+  public ModelAndView home2() {
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("redirect:/index.jsp"); // 파일 확장자 포함
+    
+    return mav;
+  }
+  
+  // http://localhost:9090/mealkit/index.do
+  /**
+   * 시작 페이지
+   * @param mno
+   * @return
+   */
+  @RequestMapping(value = "/index.do", 
+                            method = RequestMethod.GET)
+  public ModelAndView home3() {
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("/index"); // /webapp/index.jsp
+ 
+    return mav;
+  } 
+  
 }
